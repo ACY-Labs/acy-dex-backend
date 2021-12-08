95d10afd7c288139b8d50b50a4c944201f6d34b1
John Khor <john.khor97@gmail.com>
2021-12-08 13:21:38 +0800

    added launchpad backend

---
+++ src/models/launch.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const Launch = new mongoose.Schema(
+  {
+    projectName: {
+      type: String,
+      required: [true],
+      index: true,
+    },
+    projectToken: {
+      type: String,
+      required: [true],
+      index: true,
+    },
+    tokenPrice: {
+      type: Number,
+      required: [true],
+    },
+    idoDate:{
+      time: Date,
+      default: Date.now,
+    },
+    raiseSize:{
+      type: Number,
+      required: [true],
+    },
+    amount:{
+        type: Number,
+        required: [true],
+    },
+    marketCap:{
+        type: Number,
+        required: [true],
+    },
+    allocation:{
+        type: Number,
+    },
+    ticketDeposited:{
+        type: Number,
+    },
+    maxAllocation:{
+        type: Number,
+    },
+    maxWinners:{
+        type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+// convert to model
+export default mongoose.model("Launch", Launch);
