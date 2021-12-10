ed5e1fc83c49db5c5bf179e76e0e38b067adf949
austin0072009 <2001beijing@163.com>
2021-12-10 21:11:50 +0800

    launchpad index added

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import pool from "./routes/pool";
 import chart from "./routes/chart";
 import mev from "./routes/mev";
 import subscribe from "./routes/subscribe";
+import launchpad from "./routes/launchpad";
 
 // guaranteed to get dependencies
 export default () => {
@@ -11,6 +12,7 @@ export default () => {
   chart(app);
   mev(app);
   subscribe(app);
+  launchpad(app);
 
   return app;
 };
--- src/api/routes/launchpad.ts
+++ src/api/routes/launchpad.ts
@@ -11,7 +11,7 @@ export default (app: Router) => {
 
   const logger: Logger = Container.get("logger");
 
-  route.get(
+  app.get(
     "/projects",
     async (req: Request, res: Response, next: NextFunction) => {
       logger.debug(
