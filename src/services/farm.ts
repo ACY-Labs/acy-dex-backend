c5d6966ad968907c7b2ebaa8c30be7dbe85db343
nearrainbow <near_rainbow@hotmail.com>
2021-12-13 23:36:11 +0700

    test remove error

--- src/services/farm.ts
+++ src/services/farm.ts
@@ -169,7 +169,8 @@ export default class FarmService {
         const amountHex = await Promise.all(amountCol[i]).then(re => re);
         // allTokenAmount.push(amountHex);
         allTokenAmount.push(
-            amountHex.reduce((total, currentAmount) => total += parseInt(currentAmount),0 )
+            // amountHex.reduce((total, currentAmount) => total += parseInt(currentAmount),0 )
+            100
         );
     }
 
