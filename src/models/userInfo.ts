d51c6c2e6bfbf7f013ae1a52b6611f5384327391
austin0072009 <2001beijing@163.com>
2021-12-14 12:00:04 +0800

    UserInfo data base added

---
+++ src/models/userInfo.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserInfo = new mongoose.Schema(
+    {
+        walletId: {
+            type: String,
+            index: true,
+        },
+        pairs: [
+            {
+                id: { type: String },
+                token0: { type: String },
+                token1: { type: String },
+                token0deposited: {type: Number},
+                token1deposited: {type: Number},
+                
+                // holdingPercentage: { type: Number } // should be calculated on front end , because this changes every seconds
+            }
+        ],
+        totalSwappedValue: { type: Number },
+        totalFeesPaid: { type: Number },
+        totalTransactions: { type: Number },
+    },
+    { timestamps: true }
+);
+
+// convert to model
+export default mongoose.model("UserInfo", UserInfo);
