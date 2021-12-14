6b6d34243f7e216c7012239b103a72663f356537
Carlos <carlos.ramos@grupoimportia.com>
2021-12-14 17:47:05 +0800

    (userInfo) : swapAmount, TotalFee and # of transactions

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,16 +1,22 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { Container } from "typedi";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Logger } from "winston";
 import UserService from "../../services/user";
 =======
 import UserService from "../../services/user";
 import { Logger } from "winston";
 >>>>>>> 00c14da (UserInfo data base added)
+=======
+import { Logger } from "winston";
+import UserService from "../../services/user";
+>>>>>>> 56d4b13 ((userInfo) : swapAmount, TotalFee and # of transactions)
 
 const route = Router();
 
 export default (app: Router) => {
+<<<<<<< HEAD
 <<<<<<< HEAD
   app.use("/users", route);
 const userService = Container.get(UserService);
@@ -38,24 +44,32 @@ const userService = Container.get(UserService);
   // route prefix
   app.use("/userInfo", route);
 
+=======
+  app.use("/users", route);
+const userService = Container.get(UserService);
+>>>>>>> 56d4b13 ((userInfo) : swapAmount, TotalFee and # of transactions)
   const logger: Logger = Container.get("logger");
 
-  app.get(
-    "/users",
-    // async (req: Request, res: Response, next: NextFunction) => {
-    // //   logger.debug(
-    // //     "Calling chart GET endpoint /launch with query: %o",
-    // //     req.query
-    // //   );
-    // //   try {
-    // //     const launchServiceInstance = Container.get(LaunchService);
-    // //     const data = await launchServiceInstance.getProjects();
-    // //     return res.status(201).json(data);
-    // //   } catch (e) {
-    // //     logger.error("🔥 error: %o", e);
-    // //     return next(e);
-    //   }
-    // }
+  route.get(
+    "/swap",
+    async (req: Request, res: Response, next: NextFunction) => {
+      logger.debug(
+        "Calling chart POST endpoint /swap with query: %o",
+        req.query
+      );
+      try {
+        await userService.performTx(req.query);
+        return res.status(201).json({response : "Success!"});
+      } catch (e) {
+        logger.error("🔥 error: %o", e);
+        return next(e);
+      }
+    }
   );
+<<<<<<< HEAD
 };
 >>>>>>> 00c14da (UserInfo data base added)
+=======
+
+};
+>>>>>>> 56d4b13 ((userInfo) : swapAmount, TotalFee and # of transactions)
--- src/services/user.ts
+++ src/services/user.ts
@@ -49,29 +49,4 @@ export default class UserService {
                 totalTransactions: newTotalTransactions, }
             )
     }
-
-@Service()
-export default class UserService {
-  constructor(
-    @Inject("userModel") private userModel,
-    @Inject("logger") private logger
-  ) {}
-
-//   public async getProjects() {
-//     this.logger.info(`Retrieve project from db`);
-//     let data = await this.launchModel.find().exec();
-//     if(!data) 
-//         this.logger.info(`Retrieve data failed`);
-//     this.logger.debug("end getProjects");
-//     return data;
-//   }
-  public async getPair() {
-    this.logger.info(`Return Pair Infomation`);
-    // let data = await this.launchModel.find().exec();
-    // if(!data) 
-    //     this.logger.info(`Retrieve data failed`);
-    // this.logger.debug("end getProjects");
-    // return data;
-  }
-
 }
