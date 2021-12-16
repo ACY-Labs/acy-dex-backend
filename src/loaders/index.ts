7104ccda97b08da7a0fa071217ee62864797a69d
Carlos <carlos.ramos@grupoimportia.com>
2021-12-16 18:14:33 +0800

    (merge conflict)

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -40,22 +40,16 @@ export default async ({ expressApp }) => {
     model: require("../models/launch").default,
   };
 
-<<<<<<< HEAD
-=======
   const userLaunchModel = {
     name: "userLaunchModel",
     model: require("../models/userLaunch").default,
   }
 
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
   const userModel = {
     name:"userModel",
     model:require("../models/userInfo").default,
   }
-<<<<<<< HEAD
-=======
   
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
   const farmModel = {
     name: "farmModel",
     model: require("../models/farm").default,
@@ -71,10 +65,7 @@ export default async ({ expressApp }) => {
       userPoolModel,
       pairVolumeModel,
       launchModel,
-<<<<<<< HEAD
-=======
       userLaunchModel,
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
       userModel,
       farmModel
       // salaryModel,
