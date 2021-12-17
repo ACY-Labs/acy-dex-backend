2da6f60fa74a98d815220c5a5af72bcef8a13af7
凯骏 <2018080098@secoder.net>
2021-12-17 15:35:24 +0800

    (merge conflicts)

--- src/services/user.ts
+++ src/services/user.ts
@@ -17,17 +17,7 @@ export default class UserService {
             totalSwappedValue: 0,
             totalFeesPaid: 0,
             totalTransactions: 0,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             lastTransactionHash : ''
-=======
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
-=======
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
-=======
-            lastTransactionHash : ''
->>>>>>> 3401f56817e94faeb741c095b66c36e5bf210629
         });
     }
     public async performTx (data){
@@ -42,40 +32,14 @@ export default class UserService {
             await this.initUser(walletId);
             userRecord = await this.userInfoModel.findOne({ walletId }).exec();
         }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> 57eb6823a8165e5ea936d014ce5670df8b692d2b
->>>>>>> 3401f56817e94faeb741c095b66c36e5bf210629
+        
         if(userRecord.lastTransactionHash == data.hash) return;
 
         let newTotalSwappedValue =  new BigNumber(data.valueSwapped).plus(userRecord.totalSwappedValue);
         let newTotalFeesPaid = new BigNumber(data.feesPaid).plus(userRecord.totalFeesPaid) ;
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
-        let usdToken = tokenList.find(item => item.symbol == 'USDC');
-        let decimal = usdToken.decimals;
-
-        let newTotalSwappedValue =  new BigNumber(data.valueSwapped).div(new BigNumber(`1e+${decimal}`)).plus(userRecord.totalSwappedValue);
-        let newTotalFeesPaid = new BigNumber(data.feesPaid).div(new BigNumber(`1e+${decimal}`)).plus(userRecord.totalFeesPaid) ;
-<<<<<<< HEAD
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
-=======
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
-=======
-<<<<<<< HEAD
-=======
 
         let usdToken = tokenList.find(item => item.symbol == 'USDC');
         let decimal = usdToken.decimals;
->>>>>>> 57eb6823a8165e5ea936d014ce5670df8b692d2b
->>>>>>> 3401f56817e94faeb741c095b66c36e5bf210629
         let newTotalTransactions = userRecord.totalTransactions + 1;
 
         await this.userInfoModel.updateOne(
@@ -85,27 +49,9 @@ export default class UserService {
             { 
                 totalSwappedValue: newTotalSwappedValue,
                 totalFeesPaid: newTotalFeesPaid,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                 totalTransactions: newTotalTransactions,
                 lastTransactionHash : data.hash
             }
-=======
-                totalTransactions: newTotalTransactions, }
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
-=======
-                totalTransactions: newTotalTransactions, }
->>>>>>> 890647a14108276ea95025fb9ac9f5cb5d0964d3
-=======
-                totalTransactions: newTotalTransactions,
-                lastTransactionHash : data.hash
-            }
-<<<<<<< HEAD
-=======
-
->>>>>>> 57eb6823a8165e5ea936d014ce5670df8b692d2b
->>>>>>> 3401f56817e94faeb741c095b66c36e5bf210629
             )
     }
 
